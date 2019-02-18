37b0a7df4f86c1fecd879598d7c7af71821002d1
Ho Anh Tu <tuho@vn.azeus.com>
2019-02-18 16:01:01 +0700

    TuHA: registry user

--- src/main/java/com/higgsup/base/service/impl/TransactionService.java
+++ src/main/java/com/higgsup/base/service/impl/TransactionService.java
@@ -4,7 +4,6 @@ import com.higgsup.base.service.ITransactionService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Service
 @Transactional()
 public class TransactionService implements ITransactionService {
