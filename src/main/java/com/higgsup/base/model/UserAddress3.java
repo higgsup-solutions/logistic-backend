ac548d8f16d859f1ca70d0b6b5a106db1a63887b
AtulaPham <phamxuandunga@gmail.com>
2019-02-22 17:18:50 +0700

    add sample code for guide use the transformer for Projection

---
+++ src/main/java/com/higgsup/base/model/UserAddress3.java
@@ -0,0 +1,135 @@
+package com.higgsup.base.model;
+
+import java.math.BigInteger;
+
+public class UserAddress3 {
+
+    private BigInteger id;
+    private BigInteger countryId;
+    private String company;
+    private String contactName;
+    private Boolean senderDefault;
+    private Boolean receipientDefault;
+    private String address1;
+    private String address2;
+    private String cityName;
+    private String postalCode;
+    private String countryName;
+    private String stateProvince;
+    private String email;
+    private String phoneNumber;
+
+
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public BigInteger getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(BigInteger countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public Boolean getSenderDefault() {
+        return senderDefault;
+    }
+
+    public void setSenderDefault(Boolean senderDefault) {
+        this.senderDefault = senderDefault;
+    }
+
+    public Boolean getReceipientDefault() {
+        return receipientDefault;
+    }
+
+    public void setReceipientDefault(Boolean receipientDefault) {
+        this.receipientDefault = receipientDefault;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getStateProvince() {
+        return stateProvince;
+    }
+
+    public void setStateProvince(String stateProvince) {
+        this.stateProvince = stateProvince;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
--- src/main/java/com/higgsup/base/repository/impl/UserRepositoryImpl.java
+++ src/main/java/com/higgsup/base/repository/impl/UserRepositoryImpl.java
@@ -1,8 +1,11 @@
 package com.higgsup.base.repository.impl;
 
 import com.higgsup.base.model.UserAddress;
+import com.higgsup.base.model.UserAddress2;
+import com.higgsup.base.model.UserAddress3;
 import com.higgsup.base.repository.result_mapper.JpaResultConverter;
 import com.higgsup.base.repository.UserRepositoryCustom;
+import org.hibernate.transform.Transformers;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -25,5 +28,31 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
         return new JpaResultConverter().list(query,UserAddress.class);
     }
 
+
+    @Override
+    public List<UserAddress3> selectAddressListUseTransformerWithNativeQuery(Long userId) {
+
+        String queryNative = "SELECT a.id as id, a.country_id as countryId, a.company as company, a.contact_name as contactName, a.sender_default as senderDefault, a.receipient_default as receipientDefault," +
+                " a.address1 as address1, a.address2 as address2, a.city_name as cityName, a.postal_code as postalCode, co.country_name as countryName, a.state as stateProvince, a.email as email, a.phone_number as phoneNumber  " +
+                "FROM address_book a, country co where user_id = :userId  and a.country_id = co.id";
+
+        // Note: If use native query: The data type of properties of DTO class must match with data type of column of table in database
+        List<UserAddress3> userAddresses = em.createNativeQuery(queryNative).setParameter("userId", userId).unwrap( org.hibernate.query.NativeQuery.class )
+                .setResultTransformer( Transformers.aliasToBean( UserAddress3.class ) )
+                .getResultList();
+        return userAddresses;
+    }
+
+    public List<UserAddress2> selectAddressListUseTransformerWithHibernateQuery(Long userId) {
+        String queryHibernate = "SELECT a.id as id, a.countryId as countryId, a.company as company, a.contactName as contactName, a.senderDefault as senderDefault, a.receipientDefault as receipientDefault," +
+                " a.address1 as address1, a.address2 as address2, a.cityName as cityName, a.postalCode as cityName, co.countryName as countryName, a.stateProvince as stateProvince, a.email as email, a.phoneNumber as phoneNumber  " +
+                "FROM AddressBook a, Country co where a.userId = :userId and a.countryId = co.id";
+
+        // Note: If use hibernate query: The data type of properties of DTO class must match with data type of properties of entity class
+        List<UserAddress2> userAddresses = em.createQuery(queryHibernate).setParameter("userId", userId).unwrap( org.hibernate.query.Query.class )
+                .setResultTransformer( Transformers.aliasToBean( UserAddress2.class ) )
+                .getResultList();
+        return userAddresses;
+    }
 
 }
