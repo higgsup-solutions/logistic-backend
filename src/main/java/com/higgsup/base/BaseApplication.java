ef84e1af2f3f07bbeba0826bd083af23ad20a227
AtulaPham <phamxuandunga@gmail.com>
2019-03-08 09:52:10 +0700

    add plugin and change application file to build thin jar

--- src/main/java/com/higgsup/base/BaseApplication.java
+++ src/main/java/com/higgsup/base/BaseApplication.java
@@ -1,11 +1,22 @@
 package com.higgsup.base;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BaseApplication extends SpringBootServletInitializer {
+
+  @Bean
+  public ApplicationRunner runner() {
+    return app -> {
+      if (app.containsOption("fail")) {
+        throw new RuntimeException("Planned!");
+      }
+    };
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(BaseApplication.class, args);
